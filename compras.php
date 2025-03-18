cbdffec66adba668c1a0614aeab6cd128e6d1fe5
alec-boi <114313974+alec-boi@users.noreply.github.com>
2025-03-18 08:58:28 -0300

    Aula 3 - lista de compras

--- compras.php
+++ compras.php
@@ -1,7 +1,12 @@
 <?php
 
 require_once('twig-carregar.php');
+require('inc/banco.php');
+
+$dados = $pdo->query('SELECT * FROM compras');
+$comp = $dados->fetchAll(PDO::FETCH_ASSOC);
 
 echo $twig->render('compras.html', [
-    'titulo' => 'Compras'
+    'titulo' => 'Compras',
+    'compras' => $comp,
 ]);
